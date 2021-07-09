9b28ba338b705cc5188b98507a876c0df3d6d92c
kotnid <tkt0506tkt@gmail.com>
2021-07-10 01:39:20 +0800

    Update script.js

--- project_view_v2/templates/script.js
+++ project_view_v2/templates/script.js
@@ -1,5 +1,17 @@
 const ws = new WebSocket('ws://localhost/ws/msg')
 
 ws.onmessage = function(e){
-    const data = JSON.parse(e.data)
-}
+    const data = JSON.parse(e.data);
+    listMsg(data.name,data.date,data.view);
+};
+
+function listMsg(nam,date,view){
+    var data_list = document.getElementById("data_box")
+    var data = document.createElement("p");
+
+    msg = nam + " update its views to " + view + " at " + date;
+    data.innerHTML = msg;
+    data_list.appendChild(data);
+    data_list.scrollTop = data_list.scrollHeight;
+
+}; 
