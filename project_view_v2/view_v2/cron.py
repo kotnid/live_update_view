95d10025959448dd40d0a433417799eba1a87508
kotnid <tkt0506tkt@gmail.com>
2021-07-20 08:24:32 +0800

    bruh

--- project_view_v2/view_v2/cron.py
+++ project_view_v2/view_v2/cron.py
@@ -1,2 +1,17 @@
+import os 
+import json 
+
+from .get import get
+
 def get_view():
-    pass
+    get()
+    try:
+            with open("my.log") as f:
+                f = f.readlines()
+                for line in f:
+                    new_line = line[10:]
+                    data = json.dumps(eval(new_line))
+                    self.send(text_data=json.dumps(data))
+                    os.remove("my.log")
+    except:
+        pass                    
